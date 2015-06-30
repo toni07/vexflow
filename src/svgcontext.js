0d9b90a32f3872d2da980ae9b7efbdf78946d2cb
toni07 <ronchman07@yahoo.fr>
2015-06-30 16:39:14 +0200

    added console.log to see what happens

--- src/svgcontext.js
+++ src/svgcontext.js
@@ -555,6 +555,7 @@ Vex.Flow.SVGContext = (function() {
       attributes.y = y;
 
       var txt = this.create("text");
+      //this has to be modified!
       txt.textContent = text;
       this.applyAttributes(txt, attributes);
       this.svg.appendChild(txt);
